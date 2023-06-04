5a1f95e432b8505d718ce5c0c523269ab728c518
Junho Shin <shinjuno123@naver.com>
2023-06-03 17:50:44 -0700

    Feat : Add pagination to noteworthy project section

--- src/app/components/public/project/project.component.ts
+++ src/app/components/public/project/project.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Project } from 'src/app/model/project.model';
+import { DarkModeService } from 'src/app/service/dark-mode.service';
 import { ProjectService } from 'src/app/service/project.service';
 
 @Component({
@@ -7,20 +8,76 @@ import { ProjectService } from 'src/app/service/project.service';
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
-export class ProjectComponent implements OnInit{
+export class ProjectComponent implements OnInit, AfterViewChecked{
   projects: Project[] = [];
+  currentPageSize: number = 3;
+  currentPageNumber: number = 1;
+  totalPages: number[] = [];
+  isFirst: boolean = true;
+  isLast: boolean = false;
+  @ViewChild('projectContainer',{static:true}) projectContainer!: ElementRef;
 
-  constructor(private projectService: ProjectService){}
+  constructor(private projectService: ProjectService,
+    private renderer: Renderer2,
+    private darkModeService: DarkModeService,){}
 
   ngOnInit(): void {
-    this.projectService.fetchProjects().subscribe({
-      next:(projects)=>{
-        if(projects){
-          this.projects = projects;
-          console.log(this.projects);
+    this.fetchProjects();
+  }
+
+  ngAfterViewChecked(): void {
+    this.darkModeService.modeChange.subscribe(
+      isDarkMode => {
+        console.log(this.projectContainer);
+        if(isDarkMode) {
+          this.activateDarkMode();
+        } else {
+          this.deActivateDarkMode();
         }
       }
-    })
+    )
+  }
+  activateDarkMode() {
+    this.renderer.removeClass(this.projectContainer.nativeElement, "custom-bright-mode");
+    this.renderer.addClass(this.projectContainer.nativeElement, "custom-dark-mode");
+  }
+  deActivateDarkMode() {
+    this.renderer.removeClass(this.projectContainer.nativeElement, "custom-dark-mode");
+    this.renderer.addClass(this.projectContainer.nativeElement, "custom-bright-mode");
+  }
+
+  changePage(pageNumber: number){
+    this.currentPageNumber = pageNumber;
+    this.fetchProjects()
+  }
+
+  previousPage(){
+    if(this.currentPageNumber > 1){
+      this.currentPageNumber -= 1;
+      this.fetchProjects();
+    }
+  }
+
+  nextPage() {
+    if(this.currentPageNumber < this.totalPages.length){
+      this.currentPageNumber += 1;
+      this.fetchProjects();
+    }
+  }
+
+  fetchProjects(){
+    this.projectService.fetchProjects(this.currentPageSize, this.currentPageNumber).subscribe({
+      next:(projectsPage)=>{
+        if(projectsPage){
+          this.projects = projectsPage['content'];
+          this.isFirst = projectsPage['first'];
+          this.isLast = projectsPage['last'];
+          this.totalPages = Array(projectsPage['totalPages']).fill(0).map((_,idx)=>idx + 1);
+          this.currentPageNumber = projectsPage['number'] + 1;
+          this.currentPageSize = projectsPage['numberOfElements'];
+        }
+      }
+    });
   }
 
 
--- src/app/service/project.service.ts
+++ src/app/service/project.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from "@angular/core";
 import { Project } from "../model/project.model";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import { environment } from "src/environments/environment";
 import { AppConstants } from "../constants/app.constants";
 import { map } from "rxjs/operators";
+import { Router, UrlSerializer } from "@angular/router";
+import { Subject } from "rxjs";
+
 
 @Injectable({
     providedIn: "root"
 })
 export class ProjectService {
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient, 
+        private router: Router, 
+        private serializer: UrlSerializer){}
 
     // data: Project[] = [
     //     new Project("fcc85604-01aa-4083-a58e-cae70745ca1b","Fish Raising Game","https://user-images.githubusercontent.com/72008909/207324501-1b2575ce-ae42-463d-9a87-cf5e4310eb9b.png","https://www.github.com"),
@@ -17,8 +22,21 @@ export class ProjectService {
     //     new Project("fcc85604-01aa-4083-a58e-cae70745ca1b","Fish Raising Game","https://user-images.githubusercontent.com/72008909/207324501-1b2575ce-ae42-463d-9a87-cf5e4310eb9b.png","https://www.github.com")
     // ]
 
-    fetchProjects(){
-        return this.http.get<Project[]>(environment.rootUrl + AppConstants.PROJECT_API_URL,{observe:"response"})
+    changePage = new Subject<Project>();
+
+    fetchProjects(pageSize:Number, pageNumber: Number){
+        const tree = this.router.createUrlTree([AppConstants.PROJECT_API_URL],{
+            queryParams : {
+                'pageSize': pageSize,
+                'pageNumber': pageNumber
+            }
+        });
+
+        const path = this.serializer.serialize(tree);
+
+        return this.http.get<HttpResponse<any>["body"]>(environment.rootUrl + path,{observe:"response"})
         .pipe(map(response => response.body));
     }
+
+
 }
