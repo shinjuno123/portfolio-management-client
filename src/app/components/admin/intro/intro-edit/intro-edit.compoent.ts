96baa6486f8d35867b44b731125122c9f4746306
Junho Shin <shinjuno123@naver.com>
2023-06-19 10:30:34 -0700

    Feat : Add confirmation modal to intro edit screen

--- src/app/components/admin/intro/intro-edit/intro-edit.compoent.ts
+++ src/app/components/admin/intro/intro-edit/intro-edit.compoent.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
 
 
 @Component({
@@ -7,5 +7,40 @@ import { Component } from "@angular/core";
     styleUrls:['./intro-edit.component.css']
 })
 export class AdminIntroEditComponent {
+    isActivated!: boolean;
+    isConfirmed: boolean = false;
+    @ViewChild('modalButton') modalButton!:ElementRef;
+    @ViewChild('closeModalButton') closeModelButton!: ElementRef;
+
+    constructor(private renderer: Renderer2){}
+
+    activateIntroduction() {
+        if(this.isActivated) {
+            this.renderer.selectRootElement(this.modalButton.nativeElement).dispatchEvent(new Event('click'));
+        }
+    }
+
+    confirmActivation() {
+        // this.isActivated = true;
+        this.renderer.selectRootElement(this.closeModelButton.nativeElement).dispatchEvent(new Event('click'));
+        this.isConfirmed = true;
+    }
+
+    cancelActivation() {
+        this.isActivated = false;
+    }
+
+    closeModal() {
+        if(this.isConfirmed) {
+            console.log(this.isConfirmed)
+            this.isActivated = true;
+            this.isConfirmed = false;
+            return;
+        }
+
+        this.isConfirmed = false;
+        this.isActivated = false;
+        this.renderer.selectRootElement(this.closeModelButton.nativeElement).dispatchEvent(new Event('click'));
+    }
     
 }
