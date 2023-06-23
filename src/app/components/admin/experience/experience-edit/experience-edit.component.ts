ca025ce2711383a7ad2b1984c088b518ec1fcd18
Junho Shin <shinjuno123@naver.com>
2023-06-23 16:10:34 -0700

    Feat : Add experience edit page

--- src/app/components/admin/experience/experience-edit/experience-edit.component.ts
+++ src/app/components/admin/experience/experience-edit/experience-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
 
 
 @Component({
@@ -7,5 +8,59 @@ import { Component } from "@angular/core";
     styleUrls:['./experience-edit.component.css']
 })
 export class AdminExperienceEditComponent {
-    
+    isActivated!: boolean;
+    isConfirmed: boolean = false;
+    faPaperclip = faPaperclip;
+    @ViewChild('modalButton') modalButton!:ElementRef;
+    @ViewChild('closeModalButton') closeModelButton!: ElementRef;
+    @ViewChild('deleteModalButton') deleteModalButton!: ElementRef;
+
+    constructor(private renderer: Renderer2){}
+
+    activateAboutMe() {
+        if(this.isActivated) {
+            this.renderer.selectRootElement(this.modalButton.nativeElement).dispatchEvent(new Event('click'));
+        }
+    }
+
+    confirmActivation() {
+        this.renderer.selectRootElement(this.closeModelButton.nativeElement).dispatchEvent(new Event('click'));
+        this.isConfirmed = true;
+    }
+
+    cancelActivation() {
+        this.isActivated = false;
+    }
+
+    closeModal() {
+        if(this.isConfirmed) {
+            this.isActivated = true;
+            this.isConfirmed = false;
+            return;
+        }
+
+        this.isActivated = false;
+        this.renderer.selectRootElement(this.closeModelButton.nativeElement).dispatchEvent(new Event('click'));
+    }
+
+    deleteAboutMe() {
+        this.renderer.selectRootElement(this.deleteModalButton.nativeElement).dispatchEvent(new Event('click'));
+    }
+
+    submitDelete(){
+        console.log("Your data is removed!");
+    }
+
+    setAttachment(event: Event) {
+        const target = event.target as HTMLInputElement;
+        const files = target.files;
+
+        if(files !== undefined && files !== null && files.length > 0){
+            const attachment: File = files[0];
+            const pTag: ChildNode | null = target.nextSibling;
+            if(pTag) {
+                pTag.textContent = attachment.name;
+            }
+        }
+    }
 }
